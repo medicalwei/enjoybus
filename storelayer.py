de4be5b5bf28a6d8effa1d1ea7c02d80c55f460a
Mosky <mosky.tw@gmail.com>
2011-02-08 00:25:57 +0800

    changed the soft links to hard links

--- storelayer.py
+++ storelayer.py
@@ -1,1 +1,269 @@
-/home/mosky/drafts/py2/storelayer/storelayer.py
+# -*- encoding: utf-8 -*-
+# python 2.5
+ 
+from time      import time as now
+from functools import wraps
+from pickle    import dump, load, dumps, loads
+ 
+try:
+    from google.appengine.api import memcache
+    from google.appengine.ext import db
+except ImportError:
+    memcache = None
+    db = None
+ 
+def storelayer_maker(memcachelike):
+    '''Use a memcache-like object to make a store layer for functions.
+   
+   Arguments:
+       memcachelike
+           bool  set(key, value, time=0, namespace=None)
+           value get(key, namespace=None)
+           bool  delete(key, namespace=None)
+   
+   Return: <storelayer decorator maker>'''
+ 
+    def storelayer_decorator_maker(time_or_func=None, namespace=None, key_prefixer=dumps, namespace_prefixer=lambda x:x, time=0):
+        '''A maker of storelayer decorator. (or a decorator.)
+       
+        Arguments:
+            sec/func time_or_func
+                time if you want to build a decorator
+                    seconds, the data will be invalidate after this or at this
+                func if you use it as a decorator
+           
+            any namespace
+                anything the low layer accepted
+                default is the function name
+               
+            byte key_prefixer((args, kargs))
+                will be called with the arguments pass here
+                default is the pickle.dumps
+               
+            any namespace_prefixer(namespace)
+                will be called when docotrating
+                default is do nothing on namespace
+               
+            sec time
+               as the time case of time_or_func
+               
+       Return: <storelayer decorator> or <storable function>'''
+       
+        # nonlocal memcachelike
+        def storelayer_decorator(func):
+            '''A storelayer decorator.
+           
+           Return: <storable function>'''
+            # nonlocal memcachelike
+            # nonlocal time, namespace
+            ns = namespace_prefixer(namespace if namespace != None else func.func_name)
+           
+            @wraps(func)
+            def storable(*args, **kargs):
+                # nonlocal memcachelike
+                # nonlocal time, namespace
+                # nonlocal func
+                key = key_prefixer((args, kargs))
+                value = memcachelike.get(key=key, namespace=ns)
+                if value == None or kargs.get('_focus_update', False):
+                    if not func.__dict__.get('is_storable', False):
+                        try:
+                            del kargs['_focus_update']
+                        except KeyError:
+                            pass
+                    value = func(*args, **kargs)
+                    memcachelike.set(key=key, value=value, time=time, namespace=ns)
+                return value
+           
+            # install the delete method to storable 
+            storable.delete = lambda *args, **kargs: memcachelike.delete(key=key_prefixer((args, kargs)), namespace=ns)
+            storable.is_storable = True
+           
+            return storable
+           
+        if callable(time_or_func):
+            func = time_or_func
+            time = 0
+            return storelayer_decorator(time_or_func)
+        else:
+            time = time_or_func if time_or_func != None else time
+            return storelayer_decorator
+   
+    return storelayer_decorator_maker
+ 
+maker = storelayer_maker
+
+# Build a simple cacher for runtime
+# (and it is a simple example to show how to build a memcache-like instance for storelayer)
+ 
+MAX_TIME = 30*24*60*60 # a month
+ 
+class RTCache:
+    '''A runtime cacher.
+   
+   The data will be clear at the end of the program.'''
+    def __init__(self, pools=None):
+        self.pools = pools if pools != None else {}
+       
+    def set(self, key, value, time=0, namespace=None):
+        if time != 0 and time <= MAX_TIME:
+            time += now()
+   
+        pool = self.pools.get(namespace, None)
+        if pool == None:    
+            pool = self.pools[namespace] = {}
+        pool[key] = (value, time)
+       
+        return True
+ 
+    def get(self, key, namespace=None):
+        value, time = self.pools.get(namespace, {}).get(key, (None, None))
+       
+        if time != 0 and now() > time:
+            self.delete(key, namespace)
+        else:
+            return value
+           
+    def delete(self, key, namespace=None):
+        try:
+            del self.pools[namespace][key]
+            if not self.pools[namespace]:
+                del self.pools[namespace]
+        except KeyError:
+            return 1
+        else:
+            return 2
+
+    def dump(self, file):
+        dump(self.pools, file)
+
+    def load(self, file):
+        self.pools = load(file)
+ 
+rtcache = RTCache()
+rtcached = storelayer_maker(rtcache)
+ 
+if db:
+ 
+    class Datastored(db.Model):
+        namespace = db.StringProperty(multiline=True)
+        name  = db.StringProperty(multiline=True)
+        value = db.BlobProperty()
+        time  = db.FloatProperty()
+       
+    def datastore(): pass
+   
+    def _ds_get_record(key, namespace=None):
+        return iter(Datastored.gql('WHERE name = :name AND namespace = :namespace', name=key, namespace=namespace or '')).next()
+   
+    def _ds_set(key, value, time=0.0, namespace=''):
+   
+        if time != 0 and time <= MAX_TIME:
+            time += now()
+   
+        try:
+            record = datastore.get_record(key, namespace)
+        except StopIteration:
+            record = Datastored()
+       
+        record.namespace = namespace or ''
+        record.name  = key
+        record.value = dumps(value, protocol=2)
+        record.time  = float(time)
+        record.put()
+       
+        return True
+   
+    def _ds_get(key, namespace=None):
+        try:
+            record = datastore.get_record(key, namespace)
+        except StopIteration:
+            return None
+        else:
+            if record.time != 0 and now() > record.time:
+                datastore.delete(record=record)
+                value = None
+            else:
+                value = loads(str(record.value))
+            return value
+           
+    def _ds_delete(key=None, namespace=None, record=None):
+        if key == None and record == None:
+            raise TypeError('the `key` or `record` is necessary')
+        if record == None:
+            try:
+                record = datastore.get_record(key, namespace)
+            except StopIteration:
+                return 1
+        record.delete()
+        return 2
+   
+    datastore.set = _ds_set
+    datastore.get = _ds_get
+    datastore.delete = _ds_delete
+    datastore.get_record = _ds_get_record
+   
+    datastored = storelayer_maker(datastore)
+else:
+    datastored = None 
+ 
+# Make a storelayer with memcache
+memcached = None if not memcache else storelayer_maker(memcache)
+ 
+if __name__ == '__main__':
+    from time import sleep
+   
+    # in default,
+    # the namespace use the function name and here are two tests on a case,
+    # so need to define two functions
+    def test1(x): return x+c
+    def test2(x): return x+c
+   
+    # in normal case,
+    # caches will be reseted at the end of the program
+    cases = [rtcached]
+    # caches will be reseted at the close of the server
+    if memcached : cases.append(memcached)
+    # caches will not be reseted
+    if datastored: cases.append(datastored)
+  
+    for storelayer in cases:
+        case = storelayer(test1)
+        print 'Test with default parameters.'
+        c = 0; print '1st:', case(1) # 1 (if reseted else 3)
+        c = 1; print '2nd:', case(1) # 1 (if reseted else 3)
+        print 'delete:', case.delete(1) # 2 (singal of delete)
+        c = 2; print '3rd:', case(1) # 3
+        print 'focus update:'
+        c = 3; print '4th:', case(1, _focus_update=True) # 4
+        print
+       
+        case = storelayer(time=1)(test2)
+        print 'Test with time=1.'
+        c = 0; print '1st:', case(1) # 1 (always will be reset because the time limit)
+        c = 1; print '2nd:', case(1) # 1
+        print 'sleep(1)'; sleep(1)
+        c = 2; print '3rd:', case(1) # 3
+        print 'focus update:'
+        c = 3; print '4th:', case(1, _focus_update=True) # 4
+        print
+   
+    # An example for GAE environment
+    if datastored and memcached:
+        print 'Usage example:'
+         
+        @rtcached   # first, check the runtime cacher
+        @memcached  # second, check the memcache
+        @datastored # finally, check the datastore
+        def slow_func(a):
+            sleep(5)
+            return a
+   
+   
+        print 'start timer'
+        timer = now()
+   
+        print '1st:', slow_func(1)
+        print '2nd:', slow_func(1)
+           
+        print 'time costed:', now()-timer
--- unisugar.py
+++ unisugar.py
@@ -1,1 +1,143 @@
-/home/mosky/drafts/py2/unisugar/unisugar.py
+# -*- encoding: utf-8 -*-
+# confirmed it is work in python 2.6 and GAE
+# DO NOT COMMIT!
+
+import re
+import sys
+
+import chardet
+
+U_PART = re.compile(r'(?:\\u[0-9A-F]{4})+', re.I)
+X_PART = re.compile(r'(?:\\x[0-9A-F]{2})+', re.I)
+
+def uni(obj, encoding=None, unescape=True):
+    r'''decode and unescape anything to unicode.
+    
+    Argument:
+        any obj
+            any obj you want to decode into unicode.
+        string encoding
+            assign a encoding to encode this obj.
+            encoding will auto detect if it is not assigned.
+        bool unescape
+            unescape \uXXXX and \xXX if found them in obj.
+    
+    Return: unicode'''
+
+    if not isinstance(obj, basestring):
+        obj = str(obj)
+        encoding = sys.getfilesystemencoding()
+
+    if unescape:
+        if r'\x' in obj:
+            obj = X_PART.sub(lambda m: m.group().decode('string-escape'), obj)
+        if r'\u' in obj:
+            obj = uni(obj, encoding, unescape=False)
+            obj = U_PART.sub(lambda m: m.group().decode('unicode-escape'), obj)
+            return obj
+
+    if isinstance(obj, str):
+        if encoding:
+            try:
+                return obj.decode(encoding)
+            except UnicodeDecodeError:
+                pass
+        return obj.decode(chardet.detect(obj)['encoding'])
+    else:
+        return obj
+
+def cod(obj, encoding=None):
+    '''encode anything to 8-bit string.
+    
+    encdoing is set to 'utf-8' if encoding is None.
+    
+    Return: string'''
+    if not isinstance(obj, basestring):
+        return str(obj)
+
+    if encoding == None:
+        encoding = 'utf-8'
+
+    if isinstance(obj, unicode):
+        return obj.encode(encoding)
+    else:
+        return obj
+
+# url* with unicode
+#   unicode -> urlencode (using urllib.urlencode)
+#   unicode ->
+#       urlread (using urllib2.urlopen or urlfetch.fetch in GAE)
+#   -> unicode
+
+import urllib
+
+def urlencode(d, encoding=None):
+    if d != None:
+        return urllib.urlencode(dict(
+            (cod(k, encoding), cod(d[k], encoding)) for k in d
+       ))
+
+try:
+    from google.appengine.api import urlfetch
+    urllib2 = None
+except:
+    urlfetch = None
+    import urllib2
+
+from time import time
+
+def urlread(url, data=None, timeout=None, encoding=None, number=10):
+    timer = time()
+    data = urlencode(data)
+    if timeout == None: timeout = 5
+    result = None
+    while not result and number > 0:
+        print 'reading the %s ... ' % url.split('/')[-1],
+        sys.stdout.flush()
+        if urlfetch:
+            try:
+                result = urlfetch.fetch(
+                        url      = url,
+                        payload  = data,
+                        method   = urlfetch.GET if not data else urlfetch.POST,
+                        deadline = timeout
+                    ).content
+            except urlfetch.DownloadError:
+                pass
+        elif urllib2:
+            try:
+                result = urllib2.urlopen(
+                        url     = url,
+                        data    = data,
+                        timeout = timeout
+                    ).read()
+            except urllib2.URLError:
+                pass
+        print '%2.2f seconds' % (time() - timer)
+        number -= 1
+    if result:
+        return uni(result, encoding)
+
+if __name__ == '__main__':
+    print
+
+    u = u'unicode 字串'
+    s = '8-bit string 字串'
+    d = {u: s}
+    u2 = u'\xe9\x8c\xaf\xe8\xaa\xa4\xe7\x9a\x84 unicode \xe5\xad\x97\xe4\xb8\xb2'
+    s2 = '\u932f\u8aa4\u7684 8-bit string \u5b57\u4e32'
+
+    for f, m in zip(
+        (lambda x: x, uni),
+        (u'print directly:',
+         u'`uni` decoded:',
+         u'`cod` encoded:')
+    ):
+        print m
+        for obj in (u, s, d, u2, s2):
+            obj = f(obj)
+            print '    %s %s' % (type(obj), cod(obj))
+        print
+
+    #print cod(urlread('http://210.69.92.234/pda/BusStop.asp?rid=312'))
+    #print cod(urlread('http://117.56.56.194/Asp/start21.aspx', {'Glid': '275'}))
